2dcd789d66c6321ef96a434396151a76a81dbbf0
zoran <zwyliufeng@outlook.com>
2021-03-12 12:52:47 +0800

    init

---
+++ src/com/zoran/test/hashmap/Test.java
@@ -0,0 +1,14 @@
+package com.zoran.test.hashmap;
+
+import java.util.HashMap;
+import java.util.TreeMap;
+
+public class Test {
+    public static void main(String[] args) {
+        HashMap<String, Double> map = new HashMap<>();
+        map.put("k1", 0.1);
+        map.put("k2", 0.2);
+        map.put("k3", 0.3);
+        map.put("k4", 0.4);
+    }
+}
